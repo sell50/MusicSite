3be256f06a272fbe277282ddbcdbde64b4b1b4d8
Kat <pacek@uwindsor.ca>
2022-05-05 21:59:10 -0400

    add NodeJS

--- src/js/Header.js
+++
@@ -1,9 +0,0 @@
-function Header(props) {
-    return (
-        <div>
-            <img className="headerImg" src="../images/dog.jpg" />
-        </div>
-    );
-}
-
-ReactDOM.render(<Header />, document.getElementById('header'))
